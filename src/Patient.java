5aa7ee754a3f8b449ff40c8a50ec8df944a67266
soniaghanekar <soniaghanekar@gmail.com>
2013-11-14 16:32:59 -0500

    Sonia - Not showing ppl already friends

--- src/Patient.java
+++ src/Patient.java
@@ -88,7 +88,7 @@ public class Patient {
     public List<Patient> findHealthFriends(MyConnection conn) throws MyException {
         try {
             List<Patient> prospectiveFriends = new ArrayList<Patient>();
-            String query = "select DISTINCT p.pid, p.name from patient p, PatientClassRelationship pc where p.pid <> " + this.pid +
+            String query = "select DISTINCT p.pid, p.name from patient p, PatientClassRelationship pc where p.pid NOT IN (select h.fid from healthfriend h where h.pid = "+ this.pid+") and p.pid <> " + this.pid +
                     " AND p.publicStatus = 'y' " + "AND p.pid in (select DISTINCT pid from PatientClassRelationship where cid in " +
                     "(select DISTINCT cid from PatientClassRelationship where pid = "+ this.pid + " and cid <> 5))";
             ResultSet resultSet = null;
